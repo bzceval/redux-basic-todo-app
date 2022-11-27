bdc16c8f232444e03e044c229a9aac1422c4334c
bzceval <busraaceval@gmail.com>
2022-11-27 11:44:32 +0300

    fixed new redux structure

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { artır, azalt, reset } from "../redux/actions/counterAction";
 
 
 const Counter = () => {
   //store consume edilir = useSelector | react-redux
-  const counter = useSelector((state)=>state.counter)
+  // const counter = useSelector((state)=>state.counter)
+  const counter = useSelector((state) => state.count.counter)
   // action olduğunda = useDispatch() | react-redux
   const dispatch = useDispatch()
   // action = {type, payload}
@@ -13,9 +15,9 @@ const Counter = () => {
     <div>
       <h1 className="display-3 text-danger my-5">Counter</h1>
       <h5 className="display-6 mb-3">Counter: {counter}</h5>
-      <button onClick={()=>dispatch({type: "ARTIR"})} className="btn btn-dark mx-3 btn-lg">Artır</button>
-      <button onClick={()=>dispatch({type: "RESET"})} className="btn btn-danger mx-3 btn-lg">Resetle</button>
-      <button onClick={()=>dispatch({type: "AZALT"})} className="btn btn-dark mx-3 btn-lg">Azalt</button>
+      <button onClick={()=>dispatch(artır())} className="btn btn-dark mx-3 btn-lg">Artır</button>
+      <button onClick={()=>dispatch(reset())} className="btn btn-danger mx-3 btn-lg">Resetle</button>
+      <button onClick={()=>dispatch(azalt())} className="btn btn-dark mx-3 btn-lg">Azalt</button>
       
     </div>
   );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,19 +1,38 @@
-// kural 1: initial state
-export const initialState = { counter: 0 }
+import { combineReducers, legacy_createStore as createStore } from "redux";
+import counterReducer from "./reducers/counterReducer"
 
-// kural 2 : reducer oluşturulur
-// action = {type, payload}
-// const [counter, setCounter] = useState()
+// storeni kur
+// export const store = createStore()
+// reducer birleştirme = combineReducer()
+const rootReduce = combineReducers({
+    count: counterReducer
+    //eklemek istediğim reducer key: value şeklinde sadece ekle
+})
+export const store = createStore(rootReduce)
 
-export const reducer = (state=initialState, {type}) => {
-    switch (type) {
-        case "ARTIR": 
-            return {counter: state.counter + 1}
-        case "AZALT":
-            return {counter: state.counter - 1}
-        case "RESET":
-            return initialState
-        default:
-            return state
-    }
-}
+
+
+
+
+
+
+/*************/
+// // kural 1: initial state
+// export const initialState = { counter: 0 }
+// // kural 2 : reducer oluşturulur
+// // action = {type, payload}
+// // const [counter, setCounter] = useState()
+
+// export const reducer = (state=initialState, {type}) => {
+//     switch (type) {
+//         case "ARTIR": 
+//             return {counter: state.counter + 1}
+//         case "AZALT":
+//             return {counter: state.counter - 1}
+//         case "RESET":
+//             return initialState
+//         default:
+//             return state
+//     }
+// }
+
