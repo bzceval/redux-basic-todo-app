9ca0e274fae992e69c207c34d6b78c3a09bd7252
bzceval <busraaceval@gmail.com>
2022-11-27 10:28:16 +0300

    create reducer and state function

---
+++ src/redux/index.js
@@ -0,0 +1,19 @@
+// kural 1: initial state
+const initialState = { counter: 0 }
+
+// kural 2 : reducer oluşturulur
+// action = {type, payload}
+// const [counter, setCounter] = useState()
+
+const reducer = (state=initialState, {type}) => {
+    switch (type) {
+        case "ARTIR": 
+            return {counter: state.counter + 1}
+        case "AZALT":
+            return {counter: state.counter - 1}
+        case "RESET":
+            return initialState
+        default:
+            return state
+    }
+}
