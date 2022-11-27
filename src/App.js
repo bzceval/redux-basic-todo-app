dd3e17e7632729fd1eef629d87274e26edc76157
bzceval <busraaceval@gmail.com>
2022-11-27 10:37:39 +0300

    create redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import Counter from './components/Counter';
+import "./App.css";
+import Counter from "./components/Counter";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./redux";
 
 function App() {
+  const store = createStore(reducer);
   return (
     <div className="App">
-      <Counter/>
+      <Provider store={store} >
+        <Counter />
+      </Provider>
     </div>
   );
 }
 
 export default App;
+
+// store kurma adımları
+// 1. createStore() | redux
+// 2. provider |  react-redux
+// 3. consume
